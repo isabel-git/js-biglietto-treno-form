ba3fc5331dfb1ce03dc4a505959a2fe46e5939a6
Isabel <isabel.gogov1@libero.it>
2020-10-21 23:21:52 +0200

    js random, carrozza e costo biglietto

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 // Prezzo calcolato
 // Categoria selezionata dall’utente
 // Aggiungiamo una piccola animazione al click su “Crea” e “Annulla”, se clicchiamo su annulla dobbiamo ripulire il form.
+var costo;
 var tiketNome = document.getElementById("tiketNome"); // nome passeggero
 var nome = document.getElementById("campoNome");
 var el = document.getElementById("tiket");
@@ -26,13 +27,41 @@ var bottoneGen = document.getElementById("mybutton");
 
 bottoneGen.addEventListener("click",
 function() {
+  el.className = "visible";
   tiketNome.innerHTML = nome.value;
   tiketKm.innerHTML = km.value;
   tiketSelect.innerHTML = mySelect.options[mySelect.selectedIndex].value;
-  el.className = "visible";
+
+  // numero carozza
+  document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+
+  // codice del biglietto
+  document.getElementById('codiceCP').innerHTML = range(90000,100000);
+  function range(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// costo base
+costo = (0.21 * parseInt(km.value));
+console.log("costo base",costo);
+
+// condizioni eta utente
+if (mySelect.value == "Minorenne") {
+  // se l'utente è minorenne
+  costo = costo - ((costo * 20) / 100);
+  console.log("costo scontato del 20%: ",costo);
+}  else if (mySelect.value === "Over 65") {
+  // se l'utente è over 65
+  costo = costo - ((costo * 40) / 100);
+  console.log("costo scontato del 40%: ",costo);
+}
+
+// dire all'utente il costo del biglietto
+document.getElementById("costo").innerHTML = costo.toFixed(2) + "&#8364;";
 } );
 
-// bottone alulla
+// bottone annulla
 var bottoneRes = document.getElementById("myResetbutton");
 
 bottoneRes.addEventListener("click",
