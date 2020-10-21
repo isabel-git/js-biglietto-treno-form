869fc35eb3620678d8c804075bad3795b982738a
Isabel <isabel.gogov1@libero.it>
2020-10-21 17:38:26 +0200

    creazione del reset form

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@
 // Prezzo calcolato
 // Categoria selezionata dall’utente
 // Aggiungiamo una piccola animazione al click su “Crea” e “Annulla”, se clicchiamo su annulla dobbiamo ripulire il form.
-
-// nome passeggero
-var tiketNome = document.getElementById("tiketNome");
+var tiketNome = document.getElementById("tiketNome"); // nome passeggero
 var nome = document.getElementById("campoNome");
 var el = document.getElementById("tiket");
 console.log("nome",nome.value);
@@ -23,7 +21,7 @@ var tiketSelect = document.getElementById("tiketSelect")
 var mySelect = document.getElementById("mySelect");
 console.log("eta",mySelect.value);
 
-
+// bottone genera
 var bottoneGen = document.getElementById("mybutton");
 
 bottoneGen.addEventListener("click",
@@ -33,3 +31,12 @@ function() {
   tiketSelect.innerHTML = mySelect.options[mySelect.selectedIndex].value;
   el.className = "visible";
 } );
+
+// bottone alulla
+var bottoneRes = document.getElementById("myResetbutton");
+
+bottoneRes.addEventListener("click",
+  function() {
+    document.getElementById("myForm").reset();
+    el.className = "hidden";
+});
